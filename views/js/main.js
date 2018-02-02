18dd3bba5f3e3e33a655b209a7a55199ef2fcd0f
laertos <laertalickolli@gmail.com>
2018-02-02 17:29:58 -0600

    optimize pizza.html, update ReadMe

--- views/js/main.js
+++ views/js/main.js
@@ -467,8 +467,9 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+var pizzasDiv = document.getElementById("randomPizzas");
+// moved 'pizzasDiv' outside of the for loop so it only gets generated once when the page loads
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
